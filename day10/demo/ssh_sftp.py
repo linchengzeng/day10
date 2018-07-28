50de3e20921c5f7e9f884377f09d16134606362d
linchengzeng <87335610@qq.com>
2018-07-28 23:23:31 +0800

    2018年7月28日 23:23:28

--- day10/demo/ssh_sftp.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-#  Author:aling
-import paramiko
-transport = paramiko.Transport(('hostname',22))
-transport.connect(username='aling',password='aling666')#建议连接
-sftp = paramiko.SFTPClient.from_transport(transport)#定义怎么传
-sftp.put('/tmp/abc.txt','local_path')# 上传
-sftp.get('remove_path','local_path')# 下载
-transport.close()
--- day10/selectors_ftp_server/core/logger.py
+++
@@ -1,5 +0,0 @@
-# -*- coding:utf-8 -*-
-#  Author:aling
-import logging
-def logger(active):
-    logging.warning(active)
